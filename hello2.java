861b5b92c4e1032fc12c93e280c21c31961a475e
duongle1 <duongl1@go.stockton.edu>
2021-03-10 18:07:31 -0500

    Added function to hello2.java

--- hello2.java
+++ hello2.java
@@ -1,1 +1,26 @@
 
+public class Month extends Appointment
+{
+    
+    public Month (int y, int m, int d, String des)
+    {
+        super(y, m, d, des);
+    }
+
+    public boolean OccursOn(int year, int month, int day)
+    {
+        if (getMonth () == month)
+        {
+            return true;
+        }
+            else
+            {
+                return false;
+        }
+    }
+    
+    
+    
+            
+}
+
